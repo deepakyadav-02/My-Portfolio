eed098c941e46f8ed914e556ca679c3055279b62
Deepak Yadav <deepakyadav101002@gmail.com>
2023-06-24 03:59:58 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,14 +5,12 @@ import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import github from "../assets/github.png";
-import tailwind from "../assets/tailwind.png";
 import cpp from "../assets/cpp.png";
 import exp from "../assets/exp.png";
 import heroku from "../assets/heroku.png";
 import mongo from "../assets/mongo.jpeg";
 import netlify from "../assets/netlify.png";
 import node from "../assets/node.png";
-import postman from "../assets/postman.png";
 import sql from "../assets/sql.png";
 
 
@@ -61,43 +59,31 @@ const Skills = () => {
         style: "shadow-blue-400",
       },
       {
-        id: 9,
+        id: 8,
         src: sql,
         title: "MySQL",
         style: "shadow-gray-400",
       },
       {
         id: 9,
-        src: tailwind,
-        title: "Tailwind",
-        style: "shadow-yellow-400",
-      },
-      {
-        id: 10,
         src: cpp,
         title: "C++",
         style: "shadow-orange-400",
       },
       {
-        id: 11,
+        id: 10,
         src: heroku,
         title: "Heroku",
         style: "shadow-blue-400",
       },
       {
-        id: 12,
+        id: 11,
         src: netlify,
         title: "Netlify",
         style: "shadow-gray-400",
       },
       {
-        id: 13,
-        src: postman,
-        title: "Postman",
-        style: "shadow-purple-400",
-      },
-      {
-        id: 14,
+        id: 12,
         src: github,
         title: "GitHub",
         style: "shadow-pink-400",
