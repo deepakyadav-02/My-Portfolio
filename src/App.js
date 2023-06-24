b53749ebfb4f78dedef3302ca1ff2e1b38c91278
Deepak Yadav <deepakyadav101002@gmail.com>
2023-06-24 18:10:54 +0530

    trial-3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 function App() {
   return (
-    <div className="flex bg-gradient-to-r from-black via-gray-900 to-black top-0 left-0">
+    <div className="flex flex-col gap-4 bg-gradient-to-r from-black via-gray-900 to-black top-0 left-0">
       <Navbar />
       <Home />
       <Projects />
